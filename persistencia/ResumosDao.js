45c777540a56ef90b80cf32a5098dc4882ed96b0
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2018-09-16 21:50:20 -0300

    DAOS to creating coments and rating. And adjusting relationship of tbls

--- persistencia/ResumosDao.js
+++ persistencia/ResumosDao.js
@@ -2,8 +2,21 @@
         this._connection= connection;
     }
     
-        Resumos.prototype.salvaResumoLivro= function (resumo, callback) {
-            this._connection.query('INSERT INTO trabalho set ?',resumo);
+        Resumos.prototype.salvaResumoLivro = function (resumo, callback) {
+            this._connection.query("START TRANSACTION;"+
+            "insert into trabalho (tra_usu_id,tra_cat_id,tra_descricao,tra_texto,tra_dt_criacao,tra_dt_atualizacao,tra_visualizacoes)"+
+           "VALUES (1,2,testes,texto teste para inser com transaction, now(),now(),12);"+
+           "insert into livro (liv_nome,liv_editora,liv_volume,liv_ano)"+
+            "VALUES(LAST_INSERT_ID(),TESTE,testeeditora,volume 2,liv_ano);"+
+            "COMMIT;",resumo,callback);
+        }
+        Resumos.prototype.salvaResumoArtigo= function(resumo,callback){
+            this._connection.query("START TRANSACTION;"+
+                "insert into trabalho (tra_usu_id,tra_cat_id,tra_descricao,tra_texto,tra_dt_criacao,tra_dt_atualizacao,tra_visualizacoes)"+
+                "VALUES (1,2,testes,texto teste para inser com transaction, now(),now(),12);"+
+                "insert into artigo(tra_id,art_titulo)"+
+                "VALUES (LAST_INSERT_ID(),teste dois);"+
+                "COMMIT;",resumo,callback);
         }
         Resumos.prototype.listaResumosLivro= function(callback){
             this._connection.query('select t.*,l.*,u.usu_nome from trabalho t inner join livro l on t.tra_id = l.tra_id inner join usuario u on t.tra_usu_id=u.usu_id', callback);
@@ -25,6 +38,18 @@
         }
         Resumos.prototype.resumosLivroCategoria=function(id,callback){
             this._connection.query('select t.*, l.* from trabalho t inner join livro l on t.tra_id=l.tra_id where tra_cat_id = ?',id,callback);
+        }
+        Resumos.prototype.resumosAvaliacao=function(info,callback){
+            this._connection.query('insert into estrela set ?',info,callback);
+        }
+        Resumos.prototype.getAvaliacao=function(id,callback){
+            this._connection.query('select e.est_valor from estrela e where est_tra_id = ?',id,callback);
+        }
+        Resumos.prototype.newComent=function(usu_id,tra_id,comentario,callback){
+            this._connection.query('insert into comentario (comnt_usu_id,comnt_tra_id,comnt_comentario) VALUES (?,?,?)',[usu_id,tra_id,comentario],callback);
+        }
+        Resumos.prototype.getComent=function(comnt_trab_id,callback){
+            this._connection.query('select c.*,u.usu_nome,u.usu_sobrenome,u.usu_img_perfil from comentario c inner join trabalho t on c.comnt_tra_id=t.tra_id inner join usuario u on c.comnt_usu_id=u.usu_id where comnt_tra_id = ?',comnt_trab_id,callback);
         }
         
 
