daff06a1046b3d4ce028f6431569d05b0db185b7
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2018-09-23 11:18:43 -0300

    changing bcrypt lib to crypto native node lib

--- Controllers/Admin/usuariosAdmins.js
+++ Controllers/Admin/usuariosAdmins.js
@@ -1,22 +1,26 @@
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const crypto= require('crypto');
 
 module.exports=(app)=>{
     app.post("/admin/login",(req,res)=>{
         let email = req.body.usu_email;
         let senha= req.body.usu_senha;
+        let validate = false;
         const connection= app.persistencia.connectionFactory();
         const adminDao= new app.persistencia.AdminDao(connection);
         adminDao.getHash(email,(error,resultado)=>{
             if(!error){
              let hash = resultado[0].usu_senha;
-             let validade = bcrypt.compare(senha,hash);
-             if(!validade){
+             let decipher= crypto.createDecipher('aes192','teste');
+             let decrypted= decipher.update(hash,'hex','utf8');
+             decrypted+=decipher.final('utf8');
+             validate = senha==decrypted?true:false;
+             if(!validate){
                  res.status(401).json({
                      message:"Autenticação falhou"
                  });
              }
-             if(validade){
+             if(validate){
                  const token = jwt.sign({
                      email:email,
                      userId:resultado[0].usu_id
